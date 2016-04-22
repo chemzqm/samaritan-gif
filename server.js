33ada0b5f35bd221feeb86b123e337ac084d2a31
chemzqm <chemzqm@gmail.com>
2016-04-22 13:54:18 +0800

    record rawbody

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
+var fs = require('fs')
 var koa = require('koa')
 var app = koa();
 var path = require('path')
 var root = path.resolve(__dirname, 'public')
 var generate = require('./lib/generate')
+var getRawBody = require('raw-body')
 app.use(require('koa-static')(root))
 
 function genPromise(query) {
-  return promise = new Promise(function (resolve, reject) {
+  return new Promise(function (resolve, reject) {
     var o = {}
     Object.keys(query).forEach(function (k) {
       if (['width', 'height', 'fontSize']. indexOf(k) !== -1) {
@@ -38,7 +40,18 @@ function checkParams(query) {
 }
 
 app.use(function * () {
-  if (this.path == '/generate' && this.method == 'GET') {
+  if (this.path == '/return_url') {
+    var o = {
+      method: this.method,
+      query: this.query,
+      body: getRawBody(this.req, {
+        length: this.length,
+      limit: '1mb',
+      encoding: this.charset
+      })
+    }
+    yield this.recordRequest(o)
+  } else if (this.path == '/generate' && this.method == 'GET') {
     var msg = checkParams(this.query)
     if (msg) {
       this.throw(400, msg)
@@ -49,6 +62,15 @@ app.use(function * () {
     }
   }
 })
+
+function recordRequest(o) {
+  return Promise(function (resolve, reject) {
+    fs.writeFile('/home/chemzqm/return.txt', JSON.stringify(o), 'utf8', function (err) {
+      if (err) return reject(err)
+      resolve()
+    })
+  })
+}
 
 var port = process.env.PORT || 3000
 app.listen(port, '127.0.0.1')
